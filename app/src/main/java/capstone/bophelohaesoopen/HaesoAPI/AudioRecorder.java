aa2e15006980f545c9cc9e4fbf36a86dbbf9ca84
JacobNThabang <ntesangjacob@gmail.com>
2016-08-12 23:07:08 +0200

    Audio Recording startUp. - I just implemented the AundioRecorder class. -- I still need to go to the UI side as to check on the activity that wil be using this class and make necessary changes.

--- app/src/main/java/capstone/bophelohaesoopen/HaesoAPI/AudioRecorder.java
+++ app/src/main/java/capstone/bophelohaesoopen/HaesoAPI/AudioRecorder.java
@@ -1,7 +1,47 @@
 package capstone.bophelohaesoopen.HaesoAPI;
 
+import android.media.MediaRecorder;
+
+import java.io.IOException;
+
 /**
  * Created by Shaaheen on 8/8/2016.
+ * Implemented by Jacob on 12/8/2016.
+ * A class that will make audio recordings for our Haeso app.
  */
 public class AudioRecorder {
+
+    //In order to use Android MediaRecorder class, we first ned to create an instance of it.
+    MediaRecorder myAudioRecorder = new MediaRecorder();
+    private String outputFile;
+
+    public AudioRecorder(String OutPutFileName){
+        //The name of the file being created.
+        this.outputFile = OutPutFileName;
+    }
+
+    /**
+     *
+     *
+     *
+     */
+    public void recordAudio() throws IOException {
+        //To save time it takes for the recording to be in motion, this will later move to the constructor.
+        myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        myAudioRecorder.setOutputFile(outputFile);
+
+        //Prepare and start recording.
+        myAudioRecorder.prepare();
+        myAudioRecorder.start();
+    }
+
+    /**
+     * Stop recording and release the audionRecorder instance.
+     */
+    public void stopRecording() {
+        myAudioRecorder.start();
+        myAudioRecorder.release();
+    }
 }
