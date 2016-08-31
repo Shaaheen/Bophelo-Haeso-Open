e143c9213f45130986e6d3f7d9aa29c8b5576beb
Shaaheen Sacoor <sacoorshaheen@gmail.com>
2016-08-31 11:09:16 +0200

    Added identifierPrefix variable
    -Variable is used to associate media files on phone with the app

--- app/src/main/java/capstone/bophelohaesoopen/HaesoAPI/Video.java
+++ app/src/main/java/capstone/bophelohaesoopen/HaesoAPI/Video.java
@@ -11,7 +11,7 @@ public class Video extends Media {
         super(name, filePath);
     }
 
-    public Video(String name, String filePath,String thumbnailPath) {
+    public Video(String name, String filePath, String thumbnailPath) {
         super(name, filePath);
         this.thumbnailPath = thumbnailPath;
     }
