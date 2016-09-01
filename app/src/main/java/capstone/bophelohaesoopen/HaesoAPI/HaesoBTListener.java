c2e0aef4159f9c9044d58ea37841f2aea8bce2f0
Shaaheen Sacoor <sacoorshaheen@gmail.com>
2016-09-01 18:02:53 +0200

    Added more listener methods

--- app/src/main/java/capstone/bophelohaesoopen/HaesoAPI/HaesoBTListener.java
+++ app/src/main/java/capstone/bophelohaesoopen/HaesoAPI/HaesoBTListener.java
@@ -8,6 +8,8 @@ public interface HaesoBTListener{
     void onStartScan();
     void onStopScan();
     void onBTDevicesFound(List<BluetoothUtils.BTDevice> btDevices);
+    void onConnected();
+    void onDisconnected();
     void onStartReceiving();
     void onReceivingProgress(double progress);
 }
