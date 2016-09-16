1de0b9fa9e02cf527ecb1c3606c7391f15c5741b
JacobNThabang <ntesangjacob@gmail.com>
2016-09-16 03:59:16 +0200

    Created both PictureGalleryActivityTest and AudioGalleryActivityTest: - They test the start up of both PictureGalleryActivity and AudioGalleryActivity by the MainActivity using their respective buttons that lie on the UI of the MainActivity. - The test contained in either of these two classes confirms that the Picture/AudioGalleryActivity is created.

---
+++ app/src/androidTest/java/capstone/bophelohaesoopen/AudioGalleryActivityTest.java
@@ -0,0 +1,64 @@
+package capstone.bophelohaesoopen;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.widget.Button;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static junit.framework.Assert.assertNotNull;
+
+/**
+ * Created by Jacob Ntesang on 9/16/2016.
+ * This class will test the start of the AudioGalleryActivity by the MainActivity using recordings button.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public class AudioGalleryActivityTest {
+    Button recordingsButton;
+    Activity mActivity, audioGalleryActivity;
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+        mActivity = mActivityRule.getActivity();
+        recordingsButton = (Button) mActivity.findViewById(R.id.recordingsButton);
+    }
+
+    @Test
+    public void clickRecordingButton_startsAudioGlaleryActivity(){
+        // register next activity that need to be monitored.
+        Instrumentation.ActivityMonitor activityMonitor = getInstrumentation().addMonitor(AudioGalleryActivity.class.getName(), null, false);
+
+        //perform button click.
+        mActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                // click recordings button and open audioGalleryActivity.
+                recordingsButton.performClick();
+            }
+        });
+
+        //Watch for the timeout
+        //example values 5000 if in ms, or 5 if it's in seconds.
+        audioGalleryActivity = getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 5000);
+        // audioGalleryActivity is opened and captured.
+        assertNotNull(audioGalleryActivity);
+        audioGalleryActivity.finish();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        mActivity = null;
+    }
+}
+
---
+++ app/src/androidTest/java/capstone/bophelohaesoopen/PictureGalleryActivityTest.java
@@ -0,0 +1,58 @@
+package capstone.bophelohaesoopen;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.support.test.rule.ActivityTestRule;
+import android.widget.Button;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static junit.framework.Assert.assertNotNull;
+
+/**
+ * Created by Jacob Ntesang on 9/16/2016.
+ * This class will test the start of the PictureGalleryActivity by the MainActivity using pictures button.
+ */
+public class PictureGalleryActivityTest {
+    Button picturesButton;
+    Activity mActivity, pictureGalleryActivity;
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+        mActivity = mActivityRule.getActivity();
+        picturesButton = (Button) mActivity.findViewById(R.id.picturesButton);
+    }
+
+    @Test
+    public void clickRecordingButton_startsPictureGlaleryActivity(){
+        // register next activity that need to be monitored.
+        Instrumentation.ActivityMonitor activityMonitor = getInstrumentation().addMonitor(PictureGalleryActivity.class.getName(), null, false);
+
+        //perform button click.
+        mActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                // click recordings button and open audioGalleryActivity.
+                picturesButton.performClick();
+            }
+        });
+
+        //Watch for the timeout
+        //example values 5000 if in ms, or 5 if it's in seconds.
+        pictureGalleryActivity = getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 5000);
+        // audioGalleryActivity is opened and captured.
+        assertNotNull(pictureGalleryActivity);
+        pictureGalleryActivity.finish();
+    }
+    @After
+    public void tearDown() throws Exception {
+        mActivity = null;
+    }
+}
