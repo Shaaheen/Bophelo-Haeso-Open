496f157f568e3ffd43ad58d35279c66af67d4b9a
Omondi Ochieng <omondi.a.ochieng@gmail.com>
2016-10-25 16:33:34 +0200

    Added permissions to other activities

--- app/src/main/java/capstone/bophelohaesoopen/CameraActivity.java
+++ app/src/main/java/capstone/bophelohaesoopen/CameraActivity.java
@@ -1,5 +1,9 @@
 package capstone.bophelohaesoopen;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.os.Handler;
@@ -48,6 +52,17 @@ public class CameraActivity extends AppCompatActivity
         // Keep screen on
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
+        if (verifyStoragePermission())
+        {
+            setUpCamera();
+        }
+
+
+    }
+
+    private void setUpCamera()
+    {
+
         String title = getResources().getString(R.string.title_activity_camera);
         setTitle(title);
 
@@ -61,6 +76,76 @@ public class CameraActivity extends AppCompatActivity
 
         // Initialize activity view components
         initializeViews();
+
+    }
+
+    private boolean verifyStoragePermission()
+    {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED)
+        {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA))
+            {
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        3);
+
+            }
+            else
+            {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        3);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults)
+    {
+        switch (requestCode)
+        {
+            case 3: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    setUpCamera();
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else
+                {
+                    Log.w("Rec","Permission denied");
+                    onBackPressed();
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     /**
